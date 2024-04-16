8b8dadeedaa399aae191a27dd9c76b6f74e4aa8a
rajiv <rajeevkunawar007@gmail.com>
2024-04-16 18:43:40 +0100

    completed the crud for Product
    implemented following services
    
    Added code for CRUD for product
    created migrate file for product and user
    user login and register

---
+++ app/Http/Controllers/auth/AuthController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+use Session;
+use App\Models\User;
+use Hash;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+
+
+class AuthController extends Controller
+{
+    //
+     /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function index(): View
+    {
+        return view('auth.login');
+    }  
+      
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function registration(): View
+    {
+        return view('auth.registration');
+    }
+      
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function postLogin(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+   
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')
+                        ->withSuccess('You have Successfully loggedin');
+        }
+  
+        return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
+    }
+      
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function postRegistration(Request $request): RedirectResponse
+    {  
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+           
+        $data = $request->all();
+
+        $data['password'] = Hash::make($data['password']);
+
+        try {
+            User::create($data);
+            unset($data);
+            return redirect("dashboard")->withSuccess('Great! You have Successfully loggedin');
+
+        } catch ( Exception $e) {
+            unset($data);
+            return redirect()->back()->withError('Error :' . $e->getMessage());
+        }
+
+    }
+    
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function dashboard()
+    {
+        if(Auth::check()){
+            return view('users.dashboard');
+        }
+  
+        return redirect("login")->withSuccess('Opps! You do not have access');
+    }
+    
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function create(array $data)
+    {
+      return User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password'])
+      ]);
+    }
+    
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function logout(): RedirectResponse
+    {
+        Session::flush();
+        Auth::logout();
+  
+        return Redirect('login');
+    }
+
+    
+
+}
